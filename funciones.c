2999d3ed03c49bd1374fdcf22950d43ce09a749b
Marcos <marcosarios@gmail.com>
2020-07-03 05:44:24 +0100

    Initial commit

---
+++ funciones.c
@@ -0,0 +1,61 @@
+/**//* 22.333.444-PEREZ_DEL_RIO,JuanManuel-(07-2299) *//**/
+
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//**//* CUALQUIER INCLUDE DE BIBLIOTECA QUE NECESITE, HÁGALO ACÁ   *//**//**/
+
+
+/**//**//* CUALQUIER INCLUDE DE BIBLIOTECA QUE NECESITE, HÁGALO ACÁ   *//**//**/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+
+
+#include "funciones.h"
+
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//**//* ACÁ DEBE DESARROLLAR LAS FUNCIONES Y PRIMITIVAS PEDIDAS    *//**//**/
+/**//**//* ADEMÁS DE CUALQUIER OTRA FUNCIÓN QUE SE REQUIERA           *//**//**/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//* FUNCIONES Y PRIMITIVAS A DESARROLLAR                               *//**/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//* para la información                                                *//**/
+/**
+void mostrarFinal_MIO(const void *d, FILE *fp)
+{
+
+}
+ **/
+/**
+int  compararFinal_MIO(const void *d1, const void *d2)
+{
+
+}
+ **/
+
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+/**//* para el TDA LISTA                                                  *//**/
+/**
+int mostrarLista_MIO(const tLista *p,
+                     void (*mostrar)(const void *, FILE *), FILE *fp)
+{
+
+}
+ **/
+/**
+int eliminarYMostrarUnicos_MIO(tLista *p, FILE *fpPant,
+                               int comparar(const void *, const void *),
+                               void mostrar(const void *, FILE *))
+{
+
+}
+ **/
+/**
+int eliminarYMostrarRepetidos_MIO(tLista *p, FILE *fpPant,
+                                  int comparar(const void *, const void *),
+                                  void mostrar(const void *, FILE *))
+{
+
+}
+ **/
+/**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**//**/
+
